15af1d7d0ef367b5f8aa36074ea2d4039485f673
Rajkirat Singh <93331120+Itsraj3600@users.noreply.github.com>
2024-05-06 20:31:53 -0400

    modified:   Arshdeep/Id.html 	modified:   index.html 	deleted:    tempconv.css 	deleted:    tempconv.js

--- tempconv.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Code
-function CtoF() {
-    var C = document.getElementById("inC").value //get Celsius
-    var F = 1.8*C + 32 //calculate Fahrenheit
-    document.getElementById("outF").value =F //return Fahrenheit
-}
